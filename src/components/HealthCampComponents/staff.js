9e397d741e926bea5e4b313506701e1ece11df7d
Sidharth Mehta 2141038 <siidh@Sidharths-MacBook-Air.local>
2025-01-21 10:24:46 +0530

    Update Firebase configuration for HealthCamp

--- src/components/HealthCampComponents/staff.js
+++ src/components/HealthCampComponents/staff.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtWbPNRi4ia5a3pJ7aTFqqj9Z4aMN01Os",
-  authDomain: "healthcamp-f0c93.firebaseapp.com",
-  databaseURL: "https://healthcamp-f0c93-default-rtdb.firebaseio.com",
-  projectId: "healthcamp-f0c93",
-  storageBucket: "healthcamp-f0c93.firebasestorage.app",
-  messagingSenderId: "210176603069",
-  appId: "1:210176603069:web:ec38016d08a8051b1bd8d4"
+  apiKey: "AIzaSyDu5-1Ka72C5CAwIVVT924uhrjJ20u0xeo",
+  authDomain: "health-camptest.firebaseapp.com",
+  databaseURL: "https://health-camptest-default-rtdb.firebaseio.com",
+  projectId: "health-camptest",
+  storageBucket: "health-camptest.firebasestorage.app",
+  messagingSenderId: "30303192449",
+  appId: "1:30303192449:web:61135972378bd7e5491480"
 };
 
 // Initialize Firebase
